fadf34f958aa3f9bd4edebeb61720306b575500e
ludwigsaux <75636940+ludwigsaux@users.noreply.github.com>
2021-02-05 16:17:02 +0100

    ajout commentaire ajoutvoix

--- idSpeaker/ajoutvoixbd.py
+++ idSpeaker/ajoutvoixbd.py
@@ -2,28 +2,39 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import os
 
-# define the name of the directory to be created
+	#Demande le nom de la personne a enregistré
 path = input("Quel est votre nom ?")
+	#Ajoute -01 au nom afin de fonctionner avec le modeltraining
 path = path + "-01"
+	#Ouverture dossier trainingData
 os.chdir("trainingData")
+
+
 try:
     os.mkdir(path)
 except OSError:
-    print ("Creation of the directory %s failed" % path)
+    print ("Création du dossier évoucé " % path)
 else:
-    print ("Successfully created the directory %s " % path)
+    print ("Création du dossier réussi " % path)
 
-
-fs = 44100  # Sample rate
-seconds = 2  # Duration of recording
+	#Fréquence du fichier audio en Hz
+fs = 44100
+	#Durée enregistrement en seconde
+seconds = 2
+	#Enregistre
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
+	#Attend que l'enregistrement soit fini
+sd.wait()  
+	#Place l'enregistrement dans le dossier de la personne a enregistré
 os.chdir(path)
-write('output.wav', fs, myrecording)  # Save as WAV file 
+	#Fichier .wav
+write('output.wav', fs, myrecording) 
 os.chdir("../../")
+	#écrit l'emplacement du l'enregistrement
 fichier = open("trainingDataPath.txt", "w")
 fichier.write(path+"/"+"output.wav")
 fichier.close()
+	#analyse du fichier audio
 os.system('python modeltraining.py')
 
 
