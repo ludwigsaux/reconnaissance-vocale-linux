eeb7ca549f08b814739eaab7be49be3652c1ad9b
ludwigsaux <75636940+ludwigsaux@users.noreply.github.com>
2021-02-05 15:59:08 +0100

    ajout commentaire fonction auth

--- authentification.py
+++ authentification.py
@@ -1,69 +1,78 @@
 def auth(username,source,periph):
     #python3 -c 'import test; print (test.testScript("saux"))'	
 
-    #ouverture fichier log
-    import configparser
+    #import
+    import configparser #ouverture fichier ini
     import sys
+    import datetime #date temps réel
+    import importlib #appel de fonction avec des caractéres spéciaux
+    import os #permet de lancer python2 dans python3 (simule terminal)
+    import Clavier #fonction code clavier
+    
+    #ouverture fichier log
     config = configparser.ConfigParser()
     iniFile=config.read('authent.ini')
     print(iniFile)
     log = config['Log']
-    if(periph!="stdout"):
+    if(periph!="stdout"):#stdout = sortie console si autre alors on écrit dans le fichier log
         orig_stdout = sys.stdout
         f = open(str(log['log']), 'w')
         sys.stdout = f
         print("Fichier log : ", str(log['log']))
     
     
-    #date
-    import datetime
+    #date - permet d'afficher la date
     date = datetime.datetime.now()
     print("Date : ",date)
     
     #identification vocale
-    import importlib
-    import sys
-    import time
-    print(sys.version_info[0])
+        #enregistre la voix avec testvoix.py    
     Path = config['Path']
-    print(str(Path['recMic']))
     idVocale = importlib.import_module(str(Path['recMic']))
 
-    import os
+        #test la voix enregistrer avec la base de donnée
     os.chdir("idSpeaker")
     os.system('python test.py')
+        #écrit le nom de la personne trouvé
     filin = open(str(Path['data']), 'r')
     lignes = filin.readlines()
     print(lignes[0])
+        #test la personne trouvé avec la personne du qrcode
     if(lignes[0]!=username):
         print("ok !!")
         os.chdir("../")
-        #import
-        import Clavier
+        
+            #appel le fichier permettant d'écouter l'utilisateur
         Path = config['Path']
         print(str(Path['micro']))
-        vosk_api = importlib.import_module(str(Path['micro']))#appel le fichier permettant d'écouter l'utilisateur
+        vosk_api = importlib.import_module(str(Path['micro']))
         
-        
-        
-        #appel des fonctions
+            #ouvre le fichier contenant ce qu'a dit l'utilisateur
         Path = config['Path']
-        filin = open(str(Path['data']), 'r') #ouvre le fichier contenant ce qu'a dit l'utilisateur
+        filin = open(str(Path['data']), 'r') 
         lignes = filin.readlines()
-        with open(str(Path['code'])) as f:#Ouvre le fichier texte
-                    mylist = f.read().splitlines() #divise le fichier en ligne
+            #ouvre le fichier texte contenant les identifiants
+        with open(str(Path['code'])) as f:
+                        #divise le fichier en ligne
+                    mylist = f.read().splitlines() 
     
         valide=0
-        for val in mylist:#Explore chaque ligne 
-             if (val.split(";")[1]==lignes[0] and val.split(";")[0]==username ):#Test chaque ligne
+            #Explore chaque ligne 
+                #Test chaque ligne
+        for val in mylist:
+                 #compare le code dit avec le code de la db et compare le nom de la bd avec celui du qrcode
+             if (val.split(";")[1]==lignes[0] and val.split(";")[0]==username ):
                  print("mot de passe bd : ",val.split(";")[1])
                  print("mot de passe prononcé : ",lignes[0])
                  print("username bd : ",val.split(";")[0])
                  print("username : ",username)
                  print("vous pouvez passer !")
+                     #si le code dit est bon alors valide=1
                  valide=1
+                     #on sort de la boucle
                  break
-        if(valide==0): #le code saisie vocalement n'a pas été reconnu alors on entre le code au clavier
+             #le code saisie vocalement n'a pas été reconnu alors on entre le code au clavier
+        if(valide==0): 
                  code = input("code ?")
                  print("code écrit : ",code)
                  test=Clavier.clavier(code,username,source)
